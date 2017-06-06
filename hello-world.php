5dc71978a89289b39c3a5ae60560e9e17a95eda5
taga <maiko.taga@codecamp.jp>
2017-06-06 09:59:15 +0000

    first commit

---
+++ hello-world.php
@@ -0,0 +1,15 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+// echo 'Hello world from Cloud9!';
+// echo '<br>';
+// echo date('Y-m-d h:m:s');
+// echo '<br>';
+// var_dump($hoge);
+phpinfo();
+?>
+</body>
+</html>
